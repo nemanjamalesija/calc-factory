50ba4069e0fa192ae9db9a58b92d9fb46fe2d1a5
Nemanja <malesija.nemanja@gmail.com>
2023-12-10 19:06:34 +0100

    Add bare bone calculator logic

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 const CalcFactory = () => {
   return {
     firstOperand: 0,
-    secondOperand: "",
+    secondOperand: 0,
 
     add() {
       return this.firstOperand + this.secondOperand;
+    },
+
+    subtract() {
+      return this.firstOperand - this.secondOperand;
+    },
+
+    muiltiply() {
+      return this.firstOperand + this.secondOperand;
+    },
+
+    divide() {
+      return this.firstOperand / this.secondOperand;
     },
   };
 };
