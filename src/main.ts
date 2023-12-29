dd2b2c3182688706cb1c421854333e2af7b2799d
Nemanja <malesija.nemanja@gmail.com>
2023-12-29 21:55:05 +0100

    Push exercices for students

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ const calculator = new CalcFactory();
 const operator = new OperatorFactory();
 const domUpdater = new DomFactory();
 
+const prortype = Object.getPrototypeOf(calculator).constructor;
+
 btns.forEach((el) =>
   el.addEventListener("click", (e: any) => {
     calculator.updateFirstOperand(e.target!.textContent as string);
@@ -67,3 +69,189 @@ operations?.forEach((el) =>
     }
   })
 );
+
+// function initGame() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+
+//   const getPlayerScore = () => playerScore;
+//   const getComputerScore = () => computerScore;
+
+//   const IncrementPlayerScore = () => (playerScore += 1);
+//   const IncrementComputerScore = () => (computerScore += 1);
+
+//   return {
+//     getPlayerScore,
+//     getComputerScore,
+//     IncrementPlayerScore,
+//     IncrementComputerScore,
+//   };
+// }
+
+// function initGame2() {
+//   return {
+//     playerScore: 0,
+//     computerScore: 0,
+
+//     getPlayerScore() {
+//       return this.playerScore;
+//     },
+
+//     getComputedStyle() {
+//       return this.computerScore;
+//     },
+//   };
+// }
+
+// const game = initGame2();
+
+// function counterCreator() {
+//   let count = 0;
+
+//   const countIncrement = () => (count += 1);
+//   const setCount = (num) => (count = num);
+//   const getCount = () => count;
+
+//   return { countIncrement, setCount, getCount };
+// }
+
+// const root = document.querySelector(".container");
+
+// const counter = counterCreator();
+
+// counter.countIncrement();
+// counter.countIncrement();
+// counter.countIncrement();
+// counter.countIncrement();
+// counter.countIncrement();
+// counter.countIncrement();
+// counter.countIncrement();
+// counter.countIncrement();
+// counter.countIncrement();
+// counter.countIncrement();
+
+// counter.setCount(50);
+
+// root!.textContent = counter.getCount();
+
+// const eraser = document.querySelector(".eraser");
+
+// function cubeColorUpdaterCreator() {
+//   let currentColor = "red";
+
+//   const updateFieldColor = (element, color) => {
+//     element.style.backgroundColor = color;
+//   };
+
+//   const getCurrentColor = () => currentColor;
+
+//   const setCurrentColor = (newColor) => {
+//     currentColor = newColor;
+//   };
+
+//   return {
+//     updateFieldColor,
+//     getCurrentColor,
+//     setCurrentColor,
+//   };
+// }
+
+// function cubeConstructor() {
+//   const createCube = (rows, columns) => {
+//     //// logika
+//   };
+
+//   const clearCube = () => {};
+
+//   return { createCube, clearCube };
+// }
+
+// const cubeColorUpdater = cubeColorUpdaterCreator();
+// const cube = cubeConstructor();
+
+// cube.createCube(16, 16);
+
+// eraser!.addEventListener("click", () =>
+//   cubeColorUpdater.setCurrentColor("white")
+// );
+
+// setTimeout(() => console.log("timeout"), 0);
+
+// const myPromise = Promise.resolve(() => console.log("promise"));
+// myPromise.then((fn) => fn());
+
+// console.log("aaaaaaa");
+
+// for (let i = 0; i < 100; i++) {
+//   console.log("loop");
+// }
+
+// const cnt = document.querySelector(".container");
+
+// function counterCreator() {
+//   let count = 0;
+
+//   const getCount = () => count;
+//   const setCount = (val) => val;
+//   const incrementCount = () => (count += 1);
+
+//   return {
+//     getCount,
+//     setCount,
+//     incrementCount,
+//   };
+// }
+
+// const counter1 = counterCreator();
+// const counter2 = counterCreator();
+// const counter3 = counterCreator();
+
+// console.log(counter1);
+// console.log(counter2);
+// console.log(counter3);
+
+// class Marc {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// class John extends Marc {
+//   constructor(name) {
+//     super(name);
+//   }
+// }
+
+// console.log(new Marc());
+
+// console.log(new John());
+
+// const sasa = {
+//   profession: "police officerz",
+// };
+
+// const nemanja = sasa;
+// nemanja.profession = "professor";
+
+// console.log(nemanja);
+// console.log(sasa);
+
+// let sasaBroj = 5;
+// let nemanjaBroj = sasaBroj;
+
+// nemanjaBroj = 10;
+// console.log(sasaBroj);
+
+// const sir = {
+//   blir: "klir",
+//   key: {
+//     nestedKey: "nested",
+//   },
+// };
+
+// const sir2 = { ...sir, key: { ...sir.key } };
+// sir2.blir = "mutirano";
+// sir2.key.nestedKey = "mutirani nested key";
+
+// console.log(sir);
+// console.log(sir2);
